d8388d57a792f50f55856a31b72975fc21502647
Arjun Surendra <asurendra@vmware.com>
2020-08-07 21:47:07 +0530

    code

--- src/main/java/com/demo/project74/Application.java
+++ src/main/java/com/demo/project74/Application.java
@@ -1,11 +1,16 @@
 package com.demo.project74;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.vault.core.VaultKeyValueOperationsSupport;
+import org.springframework.vault.core.VaultOperations;
 import org.springframework.vault.core.VaultSysOperations;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.core.VaultTransitOperations;
@@ -14,10 +19,23 @@ import org.springframework.vault.support.VaultResponse;
 
 @SpringBootApplication
 @Slf4j
+@EnableConfigurationProperties(MySecrets.class)
 public class Application implements CommandLineRunner {
 
     @Autowired
     private VaultTemplate vaultTemplate;
+
+    @Value("${username}")
+    private String myusername;
+
+    private final MySecrets mySecrets;
+
+    @Autowired
+    private VaultOperations operations;
+
+    public Application(MySecrets mySecrets) {
+        this.mySecrets = mySecrets;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -26,10 +44,16 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... strings) {
 
-        VaultResponse response = vaultTemplate.opsForKeyValue("secret",
-                VaultKeyValueOperationsSupport.KeyValueBackend.KV_2).get("myserver");
-        log.info("Value of foo {} ", response.getData().get("foo"));
+        log.info("Value injected via @Value : {}", myusername);
 
+        log.info("Value injected via class: {}", mySecrets.getKey1());
+
+        //Reading directly.
+        VaultResponse response = vaultTemplate.opsForKeyValue("secret",
+                VaultKeyValueOperationsSupport.KeyValueBackend.KV_2).get("myapp");
+        log.info("Value of myKey: {} ", response.getData().get("myKey"));
+
+        //Writing new values to different path.
         VaultTransitOperations transitOperations = vaultTemplate.opsForTransit();
         VaultSysOperations sysOperations = vaultTemplate.opsForSys();
         if (!sysOperations.getMounts().containsKey("transit/")) {
@@ -45,4 +69,12 @@ public class Application implements CommandLineRunner {
         String plaintext = transitOperations.decrypt("foo-key", ciphertext);
         log.info("Decrypted value: {}", plaintext);
     }
+
+}
+
+@Data
+@ConfigurationProperties("group")
+class MySecrets{
+    String key1;
+    String key2;
 }
